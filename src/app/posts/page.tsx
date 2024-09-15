895d5fb1820113c21f8184bc790095e5570ecd77
Dody <prasetyodody17@gmail.com>
2024-09-15 19:44:30 +0700

    ⚡️ Improve performance by prefetching the latest post on the server
    improve performance by prefetching the latest post on the server

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,8 +1,16 @@
 import { getServerAuthSession } from "@/server/auth";
 import { LatestPosts } from "@/app/_components/latest-posts";
+import { api, HydrateClient } from "@/trpc/server";
 
 export default async function PostsPage() {
   const session = await getServerAuthSession();
 
-  return <LatestPosts session={session} />;
+  // Prefetch the latest post on the server
+  void api.post.latestPost.prefetch();
+
+  return (
+    <HydrateClient>
+      <LatestPosts session={session} />;
+    </HydrateClient>
+  );
 }
